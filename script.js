4a16d41ebd21d2c587ebfc286f53a703ce14cabc
Erika Alban <erikalban12@gmail.com>
2024-12-31 12:20:22 -0500

    adds api functionality
    * leverages axios to retrieve data from shecodes weather api
    * retrieves city name
    * retrieves temperature
    * updates html elements via newly added ids

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+function cityDetails(response) {
+  let temperature = response.data.temperature.current;
+  let tempElement = document.querySelector("#location-temperature");
+  tempElement.innerHTML = Math.round(temperature);
+
+  let cityName = response.data.city;
+  let cityElement = document.querySelector("#city-display");
+  cityElement.innerHTML = cityName;
+}
+
+function searchCity(city) {
+  let apiKey = `9b7bac1cbc64o0da36fd874214ect64b`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(cityDetails);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let inputValue = document.querySelector("#city-input");
-  let city = document.querySelector("#city-display");
-  city.innerHTML = inputValue.value;
+  let city = inputValue.value;
+  searchCity(city);
 }
 
 let searchValue = document.querySelector("#city-search");
